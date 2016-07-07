f7a49ce235221dc8cfd68d83a3929519233b329e
Daniel Cohen Gindi <Danielgindi@gmail.com>
2016-07-07 14:43:33 +0300

    Fixed processing failure for non-string values

--- i18n.js
+++ i18n.js
@@ -515,7 +515,8 @@
             
             // Process special value contents based on whether there are `options` provided,
             // or the value contains a special character
-            if (options || loc.indexOf('{') > -1 || loc.indexOf('t(') > -1) {
+            if (options || 
+                (typeof loc === 'string' && (loc.indexOf('{') > -1 || loc.indexOf('t(') > -1))) {
                 loc = i18n.processLocalizedString(loc, options);
             }
 
